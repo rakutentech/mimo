c3f6f57bb44ad4a24a91ef8866ebb38043d8c053
Graham Weldon <graham.weldon@rakuten.com>
2015-08-25 14:34:08 +0900

    Generated cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'mimo'
+maintainer       'Rakuten, Inc.'
+maintainer_email 'ops@rakops.com'
+license          'Apache 2.0'
+description      'Installs/Configures mimo'
+long_description 'Installs/Configures mimo'
+version          '0.1.0'
