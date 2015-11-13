9938552bbf5587bb6eddb0b3e133d6cb297dd08f
Graham Weldon <graham.weldon@rakuten.com>
2015-11-13 11:21:15 +0900

    Version bump to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@rakops.com'
 license          'Apache 2.0'
 description      'Installs/Configures mimo'
 long_description 'Installs/Configures mimo'
-version          '0.1.0'
+version          '0.1.1'
 
 supports 'ubuntu', '>= 12.04'
 
